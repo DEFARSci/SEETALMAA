733df4243141e9d3b1aae482d3f9084ebd14bc7a
Dammel-Meuz <ndoyemamemoussa@gmail.com>
2024-09-19 18:44:07 +0000

    moussa barre

--- script/index.js
+++ script/index.js
@@ -17,9 +17,9 @@ async function fetchPageData() {
     // console.log(article);
     // console.log(img);
     // title.innerHTML=data[0].title.rendered;
-  //  title.innerHTML=data[0].content.rendered;
+    // title.innerHTML=data[0].content.rendered;
 
-    img.innerHTML=`<img fetchpriority="high" decoding="async" class="size-full wp-image-12913 aligncenter" style="outline: 0px; letter-spacing: -0.15px;" src="http://setalmaa.com/wp-content/uploads/FIAB-Forum-International-Afrique-Setalmaa.jpeg" alt="" width="522" height="587" srcset="https://setalmaa.com/wp-content/uploads/FIAB-Forum-International-Afrique-Setalmaa.jpeg 522w, https://setalmaa.com/wp-content/uploads/FIAB-Forum-International-Afrique-Setalmaa-173x195.jpeg 173w, https://setalmaa.com/wp-content/uploads/FIAB-Forum-International-Afrique-Setalmaa-356x400.jpeg 356w, https://setalmaa.com/wp-content/uploads/FIAB-Forum-International-Afrique-Setalmaa-187x210.jpeg 187w, https://setalmaa.com/wp-content/uploads/FIAB-Forum-International-Afrique-Setalmaa-370x416.jpeg 370w" sizes="(max-width: 600px) 100vw, 522px" /></p>\n<p><span style="font-weight: 400;">`;
+    // img.innerHTML=`<img fetchpriority="high" decoding="async" class="size-full wp-image-12913 aligncenter" style="outline: 0px; letter-spacing: -0.15px;" src="http://setalmaa.com/wp-content/uploads/FIAB-Forum-International-Afrique-Setalmaa.jpeg" alt="" width="522" height="587" srcset="https://setalmaa.com/wp-content/uploads/FIAB-Forum-International-Afrique-Setalmaa.jpeg 522w, https://setalmaa.com/wp-content/uploads/FIAB-Forum-International-Afrique-Setalmaa-173x195.jpeg 173w, https://setalmaa.com/wp-content/uploads/FIAB-Forum-International-Afrique-Setalmaa-356x400.jpeg 356w, https://setalmaa.com/wp-content/uploads/FIAB-Forum-International-Afrique-Setalmaa-187x210.jpeg 187w, https://setalmaa.com/wp-content/uploads/FIAB-Forum-International-Afrique-Setalmaa-370x416.jpeg 370w" sizes="(max-width: 600px) 100vw, 522px" /></p>\n<p><span style="font-weight: 400;">`;
 
      ;
    
