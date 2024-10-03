70d81f914f531954f5ff5812a01da3662cb40976
Dammel-Meuz <ndoyemamemoussa@gmail.com>
2024-10-03 17:47:50 +0000

    diplay article

--- script/index.js
+++ script/index.js
@@ -15,40 +15,167 @@ const emailInput = document.getElementById('emailInput'); // Champ pour l'email 
 
 async function fetchPageData() {
   try {
-      // const data = await fetchPostData();
+       const data = await fetchPostData();
       
       // const post = data[0];
       
-      displayTitle('title_1',0);
-      displayContent('article_1',0); // content.rendered);
-      displayImage( 'img_1',0);
+      await displayImage(data, 'img_1',0);
+      await displayImage(data, 'img_2',5);
+      await displayImage(data, 'img_3',6);
+      await displayTitle(data,'title_1',0);
+      await displayContent(data,'article_1',0); // content.rendered);
 
       // 1
-      displayTitle('title_2',1);
+      await displayTitle(data,'title_2',1);
     
       // 2
-      displayTitle('title_3',3);
+      await displayTitle(data,'title_3',3);
       
       // 3
-      displayTitle('title_4',4);
+      await displayTitle(data,'title_4',4);
 
       // 4
-      displayImage( 'img_2',5);
-      displayTitle('title_5',5);
+      await displayTitle(data,'title_5',5);
 
       // 5
-      displayImage( 'img_3',6);
-      displayTitle('title_6',6);
+      await displayTitle(data,'title_6',6);
 
       // 6
-      displayImage('img_4',7)
-      displayTitle('title_7',7);
+      await displayImage(data,'img_4',7)
+      await displayTitle(data,'title_7',7);
       // 7
-      displayTitle('title_8',8);
+      await displayTitle(data,'title_8',8);
       // 8
-      displayTitle('title_9',9);
+      await displayTitle(data,'title_9',9);
       // 
-      displayTitle('title_10',10);
+      await displayTitle(data,'title_10',10);
+
+
+      await displayTitle(data,'title_11',40);
+      await displayTitle(data,'title_12',41);
+      await displayTitle(data,'title_13',42);
+      await displayTitle(data,'title_14',43);
+
+
+      await displayTitle(data,'first_title',11);
+      await displayImage( data,'first_img',11);
+
+      // Section-deux 1
+      await displayTitle(data,'second_title',12);
+      await displayContent(data,'first_article',12);
+      await displayImage( data,'second_img',12);
+
+      // Section-deux 2
+      await displayTitle(data,'third_title',45);
+      await displayContent(data,'second_article',45);
+      await displayImage(data, 'third_img',45);
+      
+      // Section-trois
+      await displayTitle(data,'title_un',14);
+      await displayContent(data,'article_un',14);
+      await displayImage(data, 'img_un',14);
+
+      // Section-trois 1
+      await displayTitle(data,'title_deux',15);
+      await displayImage(data, 'img_deux',15);
+
+      // Section-trois A_1
+      await displayTitle(data,'title_a1',16);
+      await displayImage( data,'img_a1',16);
+
+       // Section-trois A_2
+       await displayTitle(data,'title_a2',17);
+       await displayImage( data,'img_a2',17);
+
+        // Section-trois A_3
+        await displayTitle(data,'title_a3',18);
+        await displayImage(data, 'img_a3',18);
+
+        // Section-trois A_4
+        await displayTitle(data,'title_a4',19);
+        await displayImage(data, 'img_a4',19);
+
+        // Section-trois B
+        await displayTitle(data,'title_b',20);
+        await displayImage(data, 'img_b',20);
+        await displayContent(data,'article_b',20);
+
+        // Section-trois C
+        await displayTitle(data,'title_c',21);
+        await displayImage( data,'img_c',21);
+        await displayContent(data,'article_c',21);
+
+        // Section-quatre
+        await displayTitle(data,'title_quatre',22);
+        await displayContent(data,'article_quatre',22);
+        await displayImage( data,'img_quatre',22);
+
+        // Section-quatre A
+        await displayTitle(data,'title_4a',23);
+        await displayImage( data,'img_4a',23);
+
+        // Section-quatre B
+        await displayTitle(data,'title_4b',24);
+        await displayImage(data, 'img_4b',24);
+
+        // Section-quatre C
+        await displayTitle(data,'title_4c',25);
+        await displayImage( data,'img_4c',25);
+
+        // Section-quatre D
+        await displayTitle(data,'title_4d',26);
+        await displayImage(data, 'img_4d',26);
+        await displayContent(data,'article_4d',26);
+
+        // Section-quatre E
+        await displayTitle(data,'title_4e',27);
+        await displayImage( data,'img_4e',27);
+
+        // Section-cinq
+        await displayTitle(data,'title_cinq',28);
+        await displayContent(data,'article_cinq',28);
+        await displayImage( data,'img_cinq',28);
+
+        //Section-cinq A
+        await displayTitle(data,'title_5a',29);
+        //Section-cinq B
+        await displayTitle(data,'title_5b',30);
+        //Section-cinq C
+        await displayTitle(data,'title_5c',31);
+
+        //Section-cinq D1
+        await displayTitle(data,'title_5d1',32);
+        await displayImage( data,'img_5d1',32);
+        //Section-cinq D2
+        await displayTitle(data,'title_5d2',33);
+        await displayImage( data,'img_5d2',33);
+        //Section-cinq D3
+        await displayTitle(data,'title_5d3',32);
+        await displayImage( data,'img_5d3',32);
+        //Section-cinq D4
+        await displayTitle(data,'title_5d4',33);
+        await displayImage( data,'img_5d4',33);
+
+        //Section-cinq E1
+        await displayTitle(data,'title_5e1',34);
+        await displayImage( data,'img_5e1',34);
+
+        //Section-cinq E2
+        await displayTitle(data,'title_5e2',35);
+
+        //Section-cinq F1
+        await displayTitle(data,'title_5f1',36);
+        await displayImage(data, 'img_5f1',36);
+        
+        //Section-cinq F2
+        await displayTitle(data,'title_5f2',37);
+
+        //Section-cinq G1
+        await displayTitle(data,'title_5g1',38);
+        await displayImage(data, 'img_5g1',38);
+        
+        //Section-cinq G2
+        await displayTitle(data,'title_5g2',39);
 
       
       // await fetchCategories(post.categories);
@@ -56,6 +183,30 @@ async function fetchPageData() {
 
       // return data;
       // console.log(data);
+
+
+    //   mostvue
+    await  displayTitle(data,'most_1',0);
+    await  displayTitle(data,'most_2',1);
+    await  displayTitle(data,'most_3',2);
+    await  displayTitle(data,'most_4',3);
+    await  displayTitle(data,'most_5',4);
+    await  displayTitle(data,'most_6',5);
+    await  displayTitle(data,'most_7',6);
+    await displayTitle(data,'most_8',7);
+    await displayTitle(data,'most_9',8);
+    await displayTitle(data,'most_10',9);
+
+    await displayTitle(data,'most_1d',10);
+    await displayTitle(data,'most_2d',11);
+    await displayTitle(data,'most_3d',12);
+    await displayTitle(data,'most_4d',13);
+    await displayTitle(data,'most_5d',14);
+    await displayTitle(data,'most_6d',15);
+    await displayTitle(data,'most_7d',16);
+    await displayTitle(data,'most_8d',17);
+    await displayTitle(data,'most_9d',18);
+    await displayTitle(data,'most_10d',19);
   } catch (error) {
       console.error('Erreur lors de la récupération des données :', error);
       throw error;
@@ -64,7 +215,7 @@ async function fetchPageData() {
 
 // Fonction pour récupérer les données des posts
 async function fetchPostData() {
-  const response = await fetch('https://setalmaa.com//wp-json/wp/v2/posts?per_page=100');
+  const response = await fetch('https://setalmaa.com//wp-json/wp/v2/posts?per_page=100&page=1');
   if (!response.ok) {
       throw new Error(`Erreur HTTP ${response.status}`);
   }
@@ -91,21 +242,28 @@ async function fetchFeaturedMedia(mediaId) {
 }
 
 // Fonction pour afficher le titre
-async function displayTitle(titleid,numpost) {
-  const data = await fetchPostData();
-  const post = data[numpost];
-  const title = post.title.rendered;
-
-  document.getElementById(titleid).innerHTML = title;
-}
+async function displayTitle(data, titleid, numpost) {
+    // const data = await fetchPostData(); // Supposons que les données soient déjà disponibles
+    const post = data[numpost];
+    const title = post.title.rendered;
+    const slug = post.slug; // On récupère l'ID du post
+  
+    // Ajouter l'ID en tant qu'attribut data-id et mettre à jour le contenu
+    const titleElement = document.getElementById(titleid);
+    titleElement.innerHTML = title;
+    titleElement.setAttribute('data-id', slug);
+  
+    
+  }
+  
 
 // Fonction pour afficher le contenu de l'article
-async function displayContent(contentid,numpost) {
-  const data = await fetchPostData();
+async function displayContent(data,contentid,numpost) {
+//   const data = await fetchPostData();
   const post = data[numpost];
   const content = post.content.rendered;
   const mots = content.split(' ');
-  document.getElementById(contentid).innerHTML = mots.slice(0, 50).join(' ') + '...';
+  document.getElementById(contentid).innerHTML = mots.slice(0, 20).join(' ') + '...';
 }
 
 // Fonction pour afficher l'auteur
@@ -114,8 +272,8 @@ function displayAuthor(authorName) {
 }
 
 // Fonction pour afficher l'image à la une
-async  function displayImage(imageId,numpost) {
-  const data = await  fetchPostData();  
+async  function displayImage(data,imageId,numpost) {
+//   const data = await  fetchPostData();  
  
   const post = data[numpost];
   
@@ -259,23 +417,23 @@ async function getCategories(siteUrl) {
 
 // Utilisation de la fonction
 const siteUrl = 'https://setalmaa.com';  // Remplacer par l'URL du site WordPress
-getCategories(siteUrl);
+// getCategories(siteUrl);
 
 
 
 
 // Utilisation des fonctions
-(async function() {
-    const siteUrl = 'https://setalmaa.com';  // Remplacez par l'URL de votre site WordPress
-    const categories = await getCategories(siteUrl);
+// (async function() {
+//     const siteUrl = 'https://setalmaa.com';  // Remplacez par l'URL de votre site WordPress
+//     const categories = await getCategories(siteUrl);
     
-    // Supposons que vous souhaitez récupérer les articles de la première catégorie récupérée
-    if (categories && categories.length > 0) {
-        const categoryId = categories[0].id;  // Vous pouvez sélectionner l'ID de la catégorie que vous voulez
-        const posts = await getPostsByCategory(siteUrl, categoryId);
-        console.log(`Articles dans la catégorie "${categories[0].name}":`, posts);
-    }
-})();
+//     // Supposons que vous souhaitez récupérer les articles de la première catégorie récupérée
+//     if (categories && categories.length > 0) {
+//         const categoryId = categories[0].id;  // Vous pouvez sélectionner l'ID de la catégorie que vous voulez
+//         const posts = await getPostsByCategory(siteUrl, categoryId);
+//         console.log(`Articles dans la catégorie "${categories[0].name}":`, posts);
+//     }
+// })();
 
 // Appel Ã  la fonction principale
  // Appel de la fonction fetchPageData pour récupérer et afficher les données, y compris les images
@@ -299,20 +457,27 @@ function setupLinkRedirection(linkId) {
     // Ajouter un écouteur d'événement "click" pour rediriger l'utilisateur
     linkElement.addEventListener('click', function(event) {
         event.preventDefault(); // Empêche le comportement par défaut du lien
-
+        const slug = this.getAttribute('data-id');
+        console.log("ID du post cliqué : " + slug);
         // Récupérer le texte du lien
-        const linkText = removeAccents(linkElement.textContent);
+        // const linkText = removeAccents(linkElement.textContent);
 
-        // Transformer le texte en slug
-        const slug = stringToSlug(linkText);
-        console.log("slug :", slug);
+        // // Transformer le texte en slug
+        // const slug = stringToSlug(linkText);
+        // console.log("slug :", slug);
 
-        // Rediriger vers une URL basée sur le slug
-        window.location.href = `/article.html?slug=${slug}`; // Modifiez ce chemin si nécessaire
+        // // Rediriger vers une URL basée sur le slug
+         window.location.href = `/article.html?slug=${slug}`; // Modifiez ce chemin si nécessaire
     });
 }
 setupLinkRedirection('title_1');
 setupLinkRedirection('title_2');
+setupLinkRedirection('title_3');
+setupLinkRedirection('title_4');
+setupLinkRedirection('title_5');
+setupLinkRedirection('title_6');
+
+
 // Fonction de transformation en slug
 function stringToSlug(text) {
     return text
@@ -326,3 +491,17 @@ function stringToSlug(text) {
  function removeAccents(text) {
     return text.normalize("NFD").replace(/[\u0300-\u036f]/g, ""); // Normalise et enlève les accents
 }
+
+
+
+
+
+
+ // Masquer le GIF et afficher le body après 5 secondes
+//  setTimeout(function() {
+//     // Masquer le div de chargement
+//     document.getElementById('loading').style.display = 'none';
+
+//     // Afficher le body
+//     document.body.style.display = 'block';
+// }, 5000); // 5000ms = 5 secondes
