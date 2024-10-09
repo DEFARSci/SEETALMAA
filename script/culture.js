20c674d1147ebd0196dc9d7064dddf696b90bb94
Dammel-Meuz <ndoyemamemoussa@gmail.com>
2024-10-09 14:05:35 +0000

    page categorie

--- script/culture.js
+++ script/culture.js
@@ -1,9 +1,9 @@
 const siteUrl = 'https://setalmaa.com';  
-// const quote=document.getElementById('quote');
-// const Opinionbtn=document.getElementById('opinion');
-// const sportbtn=document.getElementById('sport');
-// const culturebtn=document.getElementById('culture');
-// const lifestylebttn=document.getElementById('lifestyle');
+const quote=document.getElementById('quote');
+const Opinionbtn=document.getElementById('opinion');
+const sportbtn=document.getElementById('sport');
+const culturebtn=document.getElementById('culture');
+const lifestylebttn=document.getElementById('lifestyle');
 
 const Headlines=['title_0','title_1','title_2','title_3','title_4','title_5','title_6',
     'title_7','title_8','title_9','title_10','title_11','title_12','title_13'];
@@ -11,7 +11,7 @@ const News_in_focus=['title_14','title_15','title_16'];
 const Spotlight=['title_17','title_18','title_19','title_20','title_21','title_22','title_23','title_24'];
 const Opinion=['title_25','title_26','title_27','title_28','title_29','title_30'];
 const sport=['title_31','title_32','title_33','title_34','title_35','title_36','title_37','title_38','title_39','title_40','title_41','title_42','title_43'];
-const allTitles = [ ...Opinion];
+const allTitles = [...News_in_focus];
 const mostView=['most_1','most_2','most_3','most_4','most_5','most_6','most_7','most_8','most_9','most_10','most_11','most_12','most_13','most_14','most_15','most_16','most_17','most_18','most_19','most_20'];
 
 // RÃ©cupÃ©ration des donnÃ©es de l'article
@@ -19,32 +19,19 @@ const mostView=['most_1','most_2','most_3','most_4','most_5','most_6','most_7','
 async function fetchPageData() {
   try {
        const data = await fetchPostData();
-     
-    //  Opinion
-    for (let i = 0; i < Opinion.length; i++) {
-        displayTitle(data, Opinion[i], 25+i);
-      }
-      await displayContent(data,'article_25',25);
-
-      await displayImage(data, 'img_25',25);
-      await displayImage(data, 'img_26',26);
-      await displayImage(data, 'img_27',27);
-      await displayImage(data, 'img_28',28);
-      await displayImage(data, 'img_29',29);
-      await displayImage(data, 'img_30',30);
-
-    //   sport
-  
-
        
+        // News_in_focus
 
-
-    //   mostvue
-
-    for (let i = 0; i < mostView.length; i++) {
-        displayTitle(data, mostView[i],i);
-        
+       for (let i = 0; i < News_in_focus.length; i++) {
+        displayTitle(data, News_in_focus[i], 14+i);
       }
+      await displayImage( data,'img_14',14);
+      await displayImage( data,'img_15',15);
+      await displayImage( data,'img_16',16);
+
+      await displayContent(data,"article_16",16);
+      await displayContent(data,"article_15",15);
+
    
   } catch (error) {
       console.error('Erreur lors de la récupération des données :', error);
@@ -62,6 +49,8 @@ async function fetchPostData() {
 }
 
 
+
+// Fonction pour récupérer les informations de l'image à la une
 async function fetchFeaturedMedia(mediaId) {
   const response = await fetch(`${siteUrl}/wp-json/wp/v2/media/${mediaId}`);
   if (!response.ok) {
@@ -82,10 +71,11 @@ async function displayTitle(data, titleid, numpost) {
     const titleElement = document.getElementById(titleid);
     titleElement.innerHTML = title;
     titleElement.setAttribute('data-id', slug);
+    
   
+    
   }
   
-
 // Fonction pour afficher le contenu de l'article
 async function displayContent(data,contentid,numpost) {
 //   const data = await fetchPostData();
@@ -95,7 +85,6 @@ async function displayContent(data,contentid,numpost) {
   document.getElementById(contentid).innerHTML = mots.slice(0, 20).join(' ') + '...';
 }
 
-// Fonction pour afficher l'auteur
 
 
 // Fonction pour afficher l'image à la une
@@ -113,37 +102,50 @@ async  function displayImage(data,imageId,numpost) {
       console.error(`L'élément avec l'ID ${imageId} n'existe pas.`);
   }
 }
-document.addEventListener('DOMContentLoaded', async () => {
-    try {
-         fetchPageData();
-    } catch (error) {
-        console.error('Erreur lors de l\'affichage des données:', error);
-    }
-  });
-      
-  
-  
-  function setupLinkRedirection(linkId) {
-      const linkElement = document.getElementById(linkId);
-      if (!linkElement) {
-          console.error(`Aucun élément trouvé avec l'ID: ${linkId}`);
-          return; // Sortir si l'élément n'existe pas
-      }
-  
-      // Ajouter un écouteur d'événement "click" pour rediriger l'utilisateur
-      linkElement.addEventListener('click', function(event) {
-          event.preventDefault(); // Empêche le comportement par défaut du lien
-          const slug = this.getAttribute('data-id');
-          console.log("ID du post cliqué : " + slug);
-        
-  
-          // // Rediriger vers une URL basée sur le slug
-           window.location.href = `../pages/article.html?slug=${slug}`; // Modifiez ce chemin si nécessaire
-      });
-  }
 
-  for (let id = 0; id < allTitles.length; id++) {
+
+ // Appel de la fonction fetchPageData pour récupérer et afficher les données, y compris les images
+ document.addEventListener('DOMContentLoaded', async () => {
+  try {
+       fetchPageData();
+  } catch (error) {
+      console.error('Erreur lors de l\'affichage des données:', error);
+  }
+});
+    
+
+
+function setupLinkRedirection(linkId) {
+    const linkElement = document.getElementById(linkId);
+    if (!linkElement) {
+        console.error(`Aucun élément trouvé avec l'ID: ${linkId}`);
+        return; // Sortir si l'élément n'existe pas
+    }
+
+    // Ajouter un écouteur d'événement "click" pour rediriger l'utilisateur
+    linkElement.addEventListener('click', function(event) {
+        event.preventDefault(); // Empêche le comportement par défaut du lien
+        const slug = this.getAttribute('data-id');
+        console.log("ID du post cliqué : " + slug);
+        
+
+        // // Rediriger vers une URL basée sur le slug
+         window.location.href = `../pages/article.html?slug=${slug}`; // Modifiez ce chemin si nécessaire
+    });
+}
+
+for (let id = 0; id < allTitles.length; id++) {
     const element = allTitles[id];
     setupLinkRedirection(element);
     
 }
+
+ // Fonction pour enlever les accents d'un texte
+
+
+
+
+
+
+
+
--- script/lifestyle.js
+++ script/lifestyle.js
@@ -1,9 +1,5 @@
 const siteUrl = 'https://setalmaa.com';  
-// const quote=document.getElementById('quote');
-// const Opinionbtn=document.getElementById('opinion');
-// const sportbtn=document.getElementById('sport');
-// const culturebtn=document.getElementById('culture');
-// const lifestylebttn=document.getElementById('lifestyle');
+
 
 const Headlines=['title_0','title_1','title_2','title_3','title_4','title_5','title_6',
     'title_7','title_8','title_9','title_10','title_11','title_12','title_13'];
@@ -11,30 +7,40 @@ const News_in_focus=['title_14','title_15','title_16'];
 const Spotlight=['title_17','title_18','title_19','title_20','title_21','title_22','title_23','title_24'];
 const Opinion=['title_25','title_26','title_27','title_28','title_29','title_30'];
 const sport=['title_31','title_32','title_33','title_34','title_35','title_36','title_37','title_38','title_39','title_40','title_41','title_42','title_43'];
-const allTitles = [ ...Opinion];
+const allTitles = [...Spotlight];
 const mostView=['most_1','most_2','most_3','most_4','most_5','most_6','most_7','most_8','most_9','most_10','most_11','most_12','most_13','most_14','most_15','most_16','most_17','most_18','most_19','most_20'];
 
 // RÃ©cupÃ©ration des donnÃ©es de l'article
 
 async function fetchPageData() {
   try {
-       const data = await fetchPostData();
+      
+
+     const data = await fetchPostData();
+
+
+    //  Spotlight
+    for (let i = 0; i < Spotlight.length; i++) {
+        displayTitle(data, Spotlight[i], 17+i);
+       
+      }
+      await displayContent(data,'article_17',17);
+      await displayContent(data,'article_23',23);
+      await displayContent(data,'article_24',24);
+      
+      await displayImage(data, 'img_17',17);
+      await displayImage(data, 'img_18',18);
+      await displayImage(data, 'img_19',19);
+      await displayImage(data, 'img_20',20);
+      await displayImage(data, 'img_21',21);
+      await displayImage(data, 'img_22',22);
+      await displayImage(data, 'img_23',23);
+      await displayImage(data, 'img_24',24);
      
     //  Opinion
-    for (let i = 0; i < Opinion.length; i++) {
-        displayTitle(data, Opinion[i], 25+i);
-      }
-      await displayContent(data,'article_25',25);
+   
 
-      await displayImage(data, 'img_25',25);
-      await displayImage(data, 'img_26',26);
-      await displayImage(data, 'img_27',27);
-      await displayImage(data, 'img_28',28);
-      await displayImage(data, 'img_29',29);
-      await displayImage(data, 'img_30',30);
-
-    //   sport
-  
+   
 
        
 
@@ -45,7 +51,7 @@ async function fetchPageData() {
         displayTitle(data, mostView[i],i);
         
       }
-   
+    
   } catch (error) {
       console.error('Erreur lors de la récupération des données :', error);
       throw error;
@@ -62,6 +68,7 @@ async function fetchPostData() {
 }
 
 
+// Fonction pour récupérer les informations de l'image à la une
 async function fetchFeaturedMedia(mediaId) {
   const response = await fetch(`${siteUrl}/wp-json/wp/v2/media/${mediaId}`);
   if (!response.ok) {
@@ -82,7 +89,9 @@ async function displayTitle(data, titleid, numpost) {
     const titleElement = document.getElementById(titleid);
     titleElement.innerHTML = title;
     titleElement.setAttribute('data-id', slug);
+    
   
+    
   }
   
 
@@ -96,7 +105,9 @@ async function displayContent(data,contentid,numpost) {
 }
 
 // Fonction pour afficher l'auteur
-
+function displayAuthor(authorName) {
+  auteur.innerHTML = authorName;
+}
 
 // Fonction pour afficher l'image à la une
 async  function displayImage(data,imageId,numpost) {
@@ -113,37 +124,41 @@ async  function displayImage(data,imageId,numpost) {
       console.error(`L'élément avec l'ID ${imageId} n'existe pas.`);
   }
 }
-document.addEventListener('DOMContentLoaded', async () => {
-    try {
-         fetchPageData();
-    } catch (error) {
-        console.error('Erreur lors de l\'affichage des données:', error);
-    }
-  });
-      
-  
-  
-  function setupLinkRedirection(linkId) {
-      const linkElement = document.getElementById(linkId);
-      if (!linkElement) {
-          console.error(`Aucun élément trouvé avec l'ID: ${linkId}`);
-          return; // Sortir si l'élément n'existe pas
-      }
-  
-      // Ajouter un écouteur d'événement "click" pour rediriger l'utilisateur
-      linkElement.addEventListener('click', function(event) {
-          event.preventDefault(); // Empêche le comportement par défaut du lien
-          const slug = this.getAttribute('data-id');
-          console.log("ID du post cliqué : " + slug);
-        
-  
-          // // Rediriger vers une URL basée sur le slug
-           window.location.href = `../pages/article.html?slug=${slug}`; // Modifiez ce chemin si nécessaire
-      });
-  }
 
-  for (let id = 0; id < allTitles.length; id++) {
+ // Appel de la fonction fetchPageData pour récupérer et afficher les données, y compris les images
+ document.addEventListener('DOMContentLoaded', async () => {
+  try {
+       fetchPageData();
+  } catch (error) {
+      console.error('Erreur lors de l\'affichage des données:', error);
+  }
+});
+    
+
+
+function setupLinkRedirection(linkId) {
+    const linkElement = document.getElementById(linkId);
+    if (!linkElement) {
+        console.error(`Aucun élément trouvé avec l'ID: ${linkId}`);
+        return; // Sortir si l'élément n'existe pas
+    }
+
+    // Ajouter un écouteur d'événement "click" pour rediriger l'utilisateur
+    linkElement.addEventListener('click', function(event) {
+        event.preventDefault(); // Empêche le comportement par défaut du lien
+        const slug = this.getAttribute('data-id');
+        console.log("ID du post cliqué : " + slug);
+       
+         window.location.href = `../pages/article.html?slug=${slug}`; // Modifiez ce chemin si nécessaire
+    });
+}
+
+for (let id = 0; id < allTitles.length; id++) {
     const element = allTitles[id];
     setupLinkRedirection(element);
     
 }
+
+
+
+
